b4918e0a85f68cdec98db7629f37057c3d81db0a
chl131 <chl131@ucsd.edu>
2023-01-04 20:50:25 -0800

    Modified header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,28 +1,32 @@
-header.innerHTML = `
+document.write(`
+
+<header id="header" class="fixed-top">
     <div class="container d-flex align-items-center justify-content-between">
 
-      <h1 class="logo"><a href="index.html">A Little Piece of Heaven for Audiophile</a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo"><img src="src/img/logo.png" alt="" class="img-fluid"></a>-->
+        <h1 class="logo"><a href="index.html">A Little Piece of Heaven for Audiophile</a></h1>
+        <!-- Uncomment below if using image logo -->
+        <!-- <a href="index.html" class="logo"><img src="src/img/logo.png" alt="" class="img-fluid"></a>-->
 
-      <nav id="navbar" class="navbar">
+        <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="./index.html">Home</a></li>
-          <li><a class="nav-link scrollto" href="./index.html#about">About</a></li>
-          <li><a class="nav-link scrollto " href="./index.html#work">Projects</a></li>
-          <li><a class="nav-link scrollto " href="./index.html#Latest">Latest</a></li>
-          <li><a class="nav-link scrollto" href="#coda">Coda</a></li>
-          <li class="dropdown"><a href="blogs/blog-home.html"><span>Blog</span> <i class="bi bi-chevron-down"></i></a>
+            <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+            <li><a class="nav-link scrollto" href="#about">About</a></li>
+            <li><a class="nav-link scrollto " href="#work">Projects</a></li>
+            <li><a class="nav-link scrollto " href="#Latest">Latest</a></li>
+            <li><a class="nav-link scrollto" href="#coda">Coda</a></li>
+            <li class="dropdown"><a href="blogs/blog-home.html"><span>Blog</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="blogs/blog-home.html">Audiophile</a></li>
-              <li><a href="blogs/blog-home.html">Melophile</a></li>
-              <li><a href="blogs/blog-home.html">Gourmet</a></li>
-              <li><a href="blogs/blog-home.html">Serendipity</a></li>
+                <li><a href="blogs/blog-home.html">Audiophile</a></li>
+                <li><a href="blogs/blog-home.html">Melophile</a></li>
+                <li><a href="blogs/blog-home.html">Gourmet</a></li>
+                <li><a href="blogs/blog-home.html">Serendipity</a></li>
             </ul>
-          </li>
+            </li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
+        </nav><!-- .navbar -->
 
     </div>
-`
+</header>
+
+`);
